b7fe5555b22ad64e800f4703b6d2d85797852998
Thiago Carvalho de Souza <102882911+Thiago-cz@users.noreply.github.com>
2024-03-23 11:34:07 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -18,6 +18,6 @@ server.use(/^(?!\/(public|livros|autores|categorias)).*$/, authenticationMiddlew
 server.use(router) //rotas na prota 3000
 
 server.listen(8000, () => {
-  console.log("Bem vindo a API do Alura Books")
+  console.log("Bem vindo a API do Alura Books!")
   console.log("API disponível em http://localhost:8000")
 })
